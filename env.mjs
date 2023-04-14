3f366195b1c6992423d8dc32fce1e229b16fd9cc
SavelevMatthew <savelevmatthew@gmail.com>
2023-04-15 02:56:14 +0500

    feat: added env validation

---
+++ env.mjs
@@ -0,0 +1,63 @@
+import { z } from 'zod'
+
+const server = z.object({
+    DATABASE_URL: z.string().min(1),
+    SHADOW_DATABASE_URL: z.string().min(1),
+    NODE_ENV: z.enum(['development', 'production', 'test'])
+})
+
+const client = z.object({
+
+})
+
+const merged = server.merge(client)
+
+/**
+ * You can't destruct `process.env` as a regular object in the Next.js edge runtimes (e.g.
+ * middlewares) or client-side, so we need to destruct manually.
+ *
+ * @type {Record<keyof z.infer<typeof server> | keyof z.infer<typeof client>, string | undefined>}
+ */
+const processEnv = {
+    NODE_ENV: process.env.NODE_ENV,
+    DATABASE_URL: process.env.DATABASE_URL,
+    SHADOW_DATABASE_URL: process.env.SHADOW_DATABASE_URL,
+}
+
+/** @typedef {z.input<typeof merged>} MergedInput */
+/** @typedef {z.infer<typeof merged>} MergedOutput */
+/** @typedef {z.SafeParseReturnType<MergedInput, MergedOutput>} MergedSafeParseReturn */
+
+let env = /** @type {MergedOutput} */ (process.env)
+
+if (typeof process.env.SKIP_ENV_VALIDATION !== 'string' || process.env.SKIP_ENV_VALIDATION.toLowerCase() !== 'false') {
+    const isServer = typeof window === 'undefined'
+    const parsed = /** @type {MergedSafeParseReturn} */ isServer
+        ? merged.safeParse(processEnv)
+        : client.safeParse(processEnv)
+
+    if (parsed.success === false) {
+        console.error(
+            "❌ Invalid environment variables:",
+            parsed.error.flatten().fieldErrors,
+        );
+        throw new Error("Invalid environment variables");
+    }
+
+    env = /** @type {MergedOutput} */ (new Proxy(parsed.data, {
+        get(target, prop) {
+            if (typeof prop !== "string") return undefined;
+            // Throw a descriptive error if a server-side env var is accessed on the client
+            // Otherwise it would just be returning `undefined` and be annoying to debug
+            if (!isServer && !prop.startsWith("NEXT_PUBLIC_"))
+                throw new Error(
+                    process.env.NODE_ENV === "production"
+                        ? "❌ Attempted to access a server-side environment variable on the client"
+                        : `❌ Attempted to access server-side environment variable '${prop}' on the client`,
+                );
+            return target[/** @type {keyof typeof target} */ (prop)];
+        },
+    }))
+}
+
+export { env }
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
+!process.env.SKIP_ENV_VALIDATION && (await import('./env.mjs'))
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
 }
 
-module.exports = nextConfig
+export default nextConfig
