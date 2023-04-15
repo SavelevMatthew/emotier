52c516d1b6ede8af1a567ad2b421e99ad7c4b5fc
SavelevMatthew <savelevmatthew@gmail.com>
2023-04-15 12:45:57 +0500

    feat: switched to appDir

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,11 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    experimental: {
+        appDir: true
+    },
     reactStrictMode: true,
+    swcMinify: true
 }
 
 export default nextConfig
